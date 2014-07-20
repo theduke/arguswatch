9ca87d8eeb18c724a8085fa512923aea007e4c2c
theduke <chris@theduke.at>
2014-07-20 14:51:16 +0200

    initial work on api module

---
+++ argus_api/serializers.py
@@ -0,0 +1,16 @@
+from django.forms import widgets
+from rest_framework import serializers
+
+from ..argus_services.models import Service
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id',
+            'name', 'description', 'notes', 'tags',
+            'parent', 'groups',
+            'enabled',
+            'state_type', 'state',
+            'last_checked', 'last_ok', 'last_state_change',
+        )
